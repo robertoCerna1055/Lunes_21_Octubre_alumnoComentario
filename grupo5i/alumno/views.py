a67fdf773f80ce372734812fd35c02a6a3ff3e09
Roberto Cerna <164534193+robertoCerna1055@users.noreply.github.com>
2024-10-21 19:39:56 -0600

    Add files via upload

---
+++ grupo5i/alumno/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Alumno
+
+# Create your views here.
+
+def index_vista(request):
+    misalumnos=Alumno.objects.all().order_by("id")
+    return render(request,"index.html",{"misalumnos":misalumnos})
+
+
+def Alumno_vista(request,id):
+    alumno = get_object_or_404(Alumno,id=id)
+    return render(request, 'alumno.html',{'objeto':alumno})
